43123199f0f092a2bacdc2b49ed93b55b2774445
Shashank Naik <shashanknaik808@gmail.com>
2023-12-05 11:38:53 +0530

    Added MUI Tabs component with three Tab labels: First, Second, and Third

--- mui-tut/src/Test.jsx
+++ mui-tut/src/Test.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Tabs, Tab } from '@mui/material';
 
 function Test() {
     return (
-        <div>Test</div>
+        <div>
+            <Tabs indicatorColor="secondary" textColor="secondary" value={value} onChange={(e, val) => setValue(val)}>
+                <Tab label="First" />
+                <Tab label="Second" />
+                <Tab label="Third" />
+            </Tabs>
+        </div>
     )
 }
 
